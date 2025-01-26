62a79e4cbb88a4d1522a04181ebfd2ddb595f057
aditya <aditya.n.daw@gmail.com>
2025-01-26 10:54:51 +0530

    Removing un important lines

--- main.py
+++ main.py
@@ -11,17 +11,9 @@ import matplotlib.pyplot as plt
 from algorithm.indicators import *
 
 if __name__ == '__main__':
-    mysql = Procedure()
     start_secs = '17-06-2024 00:00:00'
     end_secs = '17-08-2024 00:00:00'
     ret = get_time_series_data(start_secs,end_secs,interval=5)
     my_data = ret.values.astype('f')
-    my_data = on_neck_signal(my_data)
-    org_data = pd.DataFrame(my_data)
-    ohlc_plot_candles_main(my_data)
-    print(my_data)
-    my_data = org_data.loc[org_data[5] == -1]
-    print(my_data)
-    my_data = org_data.loc[org_data[4] == 1]
     print(my_data)
     print(ret)
